4dddb146112824aff23ec86ec3fc3572d2c913e9
RafaelGSS <rafael.nunu@hotmail.com>
2023-01-25 00:14:54 -0300

    feat: include cli colors and ascii message

---
+++ ascii.js
@@ -0,0 +1,35 @@
+const clc = require('cli-color');
+
+const danger = `
+                                                   
+██████   █████  ███    ██  ██████  ███████ ██████  
+██   ██ ██   ██ ████   ██ ██       ██      ██   ██ 
+██   ██ ███████ ██ ██  ██ ██   ███ █████   ██████  
+██   ██ ██   ██ ██  ██ ██ ██    ██ ██      ██   ██ 
+██████  ██   ██ ██   ████  ██████  ███████ ██   ██ 
+                                                   
+`;
+
+const allGood = `
+                                                                          
+ █████  ██      ██           ██████   ██████   ██████  ██████         ██  
+██   ██ ██      ██          ██       ██    ██ ██    ██ ██   ██     ██  ██ 
+███████ ██      ██          ██   ███ ██    ██ ██    ██ ██   ██         ██ 
+██   ██ ██      ██          ██    ██ ██    ██ ██    ██ ██   ██     ██  ██ 
+██   ██ ███████ ███████      ██████   ██████   ██████  ██████         ██  
+                                                                          
+`;
+
+const bold = clc.bold;
+
+const vulnerableWarning = bold(`The current Node.js version (${process.version}) is vulnerable to the following CVEs:`);
+
+const separator = '='.repeat(process.stdout.columns);
+
+module.exports = {
+  danger,
+  allGood,
+  bold,
+  vulnerableWarning,
+  separator,
+};
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { request, stream, setGlobalDispatcher, Agent } = require('undici');
 const EE = require('node:events');
 const fs = require('node:fs');
 const path = require('node:path');
-const semver = require('semver');
+const debug = require('debug')('is-my-node-vulnerable');
+const satisfies = require('semver/functions/satisfies');
+const { danger, vulnerableWarning, bold, separator, allGood } = require('./ascii');
 
 setGlobalDispatcher(new Agent({ connections: 20 }));
 
@@ -14,7 +16,7 @@ const coreLocalFile = path.join(__dirname, 'core.json');
 const ETagFile = path.join(__dirname, '.etag');
 
 async function readLocal(file) {
-  return fs.createReadStream(file);
+  return require(file);
 }
 
 function loadETag() {
@@ -45,24 +47,40 @@ async function getCoreIndex() {
   const { headers } = await request(CORE_RAW_URL, { method: 'HEAD' });
   if (!lastETagValue || lastETagValue !== headers['etag']) {
     updateLastETag(headers['etag']);
-    console.info('Creating local core.json');
-    // TODO: create local
+    debug('Creating local core.json');
     return fetchCoreIndex();
   } else {
-    console.info('No updates from upstream. Getting a cached version.');
+    debug('No updates from upstream. Getting a cached version.');
     return readLocal(coreLocalFile);
   }
+}
+
+function getVulnerabilityList(data) {
+  const list = [];
+  for (const key in data) {
+    const vuln = data[key];
+    if (
+      satisfies(process.version, vuln.vulnerable) &&
+      !satisfies(process.version, vuln.patched)
+    ) {
+      list.push(`${bold(vuln.cve)}: ${vuln.overview}\n${bold('Patched versions')}: ${vuln.patched}`)
+    }
+  }
+  return list;
 }
 
 async function main() {
   loadETag();
   const coreIndex = await getCoreIndex();
-  const JSONTransformStream = new TransformStream({
-    transform: (chunk, controller) => {
-      controller.enqueue(chunk);
-    }
-  })
-  coreIndex.pipe(JSONTransformStream)
+  const list = getVulnerabilityList(coreIndex);
+  if (list.length) {
+    console.error(danger);
+    console.error(vulnerableWarning + '\n');
+    console.error(`${list.join(`\n${separator}\n\n`)}\n${separator}`);
+    process.exit(1);
+  } else {
+    console.info(allGood);
+  }
 }
 
 main();
