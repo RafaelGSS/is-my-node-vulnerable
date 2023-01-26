4dade2265fbae46a05a4eceb7102057c54d63efe
RafaelGSS <rafael.nunu@hotmail.com>
2023-01-25 22:00:43 -0300

    fix: lint

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const assert = require('node:assert')
 const { isNodeVulnerable } = require('./index')
 
-async function t() {
+async function t () {
   assert.ok(await isNodeVulnerable('19.0.0'))
   assert.ok(await isNodeVulnerable('18.0.0'))
   assert.ok(await isNodeVulnerable('14.0.0'))
