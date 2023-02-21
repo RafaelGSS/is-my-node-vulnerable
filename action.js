1e8457ae05c1ab27765d2dee53fb15284cf098fa
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-02-21 09:34:47 +0100

    fix: properly manage async

--- action.js
+++ action.js
@@ -1,14 +1,14 @@
-const core = require("@actions/core");
+const core = require('@actions/core')
 const { isNodeVulnerable } = require('./index')
 
 async function run () {
-
   // Inputs
   const nodeVersion = core.getInput('node-version', { required: true })
-  if(isNodeVulnerable(nodeVersion)){
-    core.setFailed(`Node.js version ${nodeVersion} is vulnerable`)
-  }
 
+  const isVulnerable = await isNodeVulnerable(nodeVersion)
+  if (isVulnerable) {
+    core.setFailed(`Node.js version ${nodeVersion} is vulnerable. Please upgrade!`)
+  }
 }
 
 run()
