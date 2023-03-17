6acd1ff47f835e4b09120ee77b33d2df6c5cd60d
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-03-17 15:50:47 +0100

    feat: added affectedEnvironments validation

--- action.js
+++ action.js
@@ -4,8 +4,14 @@ const { isNodeVulnerable } = require('./index')
 async function run () {
   // Inputs
   const nodeVersion = core.getInput('node-version', { required: true })
-  core.info(`Checking Node.js version ${nodeVersion}...`)
-  const isVulnerable = await isNodeVulnerable(nodeVersion)
+  const platform = core.getInput('platform', { required: false })
+
+  if (platform && !['linux', 'win', 'osx'].includes(platform)) {
+    core.setFailed(`platform ${platform} is not valid. Please use linux, win or osx.`)
+  }
+
+  core.info(`Checking Node.js version ${nodeVersion} with platform ${platform}...`)
+  const isVulnerable = await isNodeVulnerable(nodeVersion, platform)
   if (isVulnerable) {
     core.setFailed(`Node.js version ${nodeVersion} is vulnerable. Please upgrade!`)
   } else {
