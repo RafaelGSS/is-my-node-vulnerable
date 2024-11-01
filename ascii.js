385d733de3bf71cab507754dba5878c676a86e20
Rafael Gonzaga <rafael.nunu@hotmail.com>
2024-11-01 13:10:11 -0300

    feat: add support to old versions of Node.js (#23)
    * feat: add compatibility to Node.js >= 0.12
    
    For retro-compatibility lovers
    
    * doc: add note about supported Node.js versions
    
    * fixup! feat: add compatibility to Node.js >= 0.12
    
    * chore: drop cli-colors
    
    Refs: https://github.com/RafaelGSS/is-my-node-vulnerable/issues/19
    
    * fixup! extra forward slashes
    
    Co-authored-by: Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
    
    * fixup! feat: add compatibility to Node.js >= 0.12
    
    ---------
    
    Co-authored-by: Trivikram Kamat <16024985+trivikr@users.noreply.github.com>

--- ascii.js
+++ ascii.js
@@ -1,35 +1,45 @@
-const clc = require('cli-color')
+const util = require('util')
 
-const danger = `
-                                                   
-██████   █████  ███    ██  ██████  ███████ ██████  
-██   ██ ██   ██ ████   ██ ██       ██      ██   ██ 
-██   ██ ███████ ██ ██  ██ ██   ███ █████   ██████  
-██   ██ ██   ██ ██  ██ ██ ██    ██ ██      ██   ██ 
-██████  ██   ██ ██   ████  ██████  ███████ ██   ██ 
-                                                   
-`
+const danger = '\n' +
+'\n' +
+'██████   █████  ███    ██  ██████  ███████ ███████\n' +
+'██   ██ ██   ██ ████   ██ ██       ██      ██   ██\n' +
+'██   ██ ███████ ██ ██  ██ ██   ███ █████   ███████\n' +
+'██   ██ ██   ██ ██  ██ ██ ██    ██ ██      ██   ██\n' +
+'██████  ██   ██ ██   ████  ██████  ███████ ██   ██\n' +
+'\n'
 
-const allGood = `
-                                                                          
- █████  ██      ██           ██████   ██████   ██████  ██████         ██  
-██   ██ ██      ██          ██       ██    ██ ██    ██ ██   ██     ██  ██ 
-███████ ██      ██          ██   ███ ██    ██ ██    ██ ██   ██         ██ 
-██   ██ ██      ██          ██    ██ ██    ██ ██    ██ ██   ██     ██  ██ 
-██   ██ ███████ ███████      ██████   ██████   ██████  ██████         ██  
-                                                                          
-`
+const allGood = '\n' +
+'\n' +
+' █████  ██      ██           ██████   ██████   ██████  ██████         ██\n' +
+'██   ██ ██      ██          ██       ██    ██ ██    ██ ██   ██     ██  ██\n' +
+'███████ ██      ██          ██   ███ ██    ██ ██    ██ ██   ██         ██\n' +
+'██   ██ ██      ██          ██    ██ ██    ██ ██    ██ ██   ██     ██  ██\n' +
+'██   ██ ███████ ███████      ██████   ██████   ██████  ██████         ██\n' +
+'\n'
 
-const bold = clc.bold
-
-const vulnerableWarning = bold(`The current Node.js version (${process.version}) is vulnerable to the following CVEs:`)
-
-const separator = '='.repeat(process.stdout.columns)
-
-module.exports = {
-  danger,
-  allGood,
-  bold,
-  vulnerableWarning,
-  separator
+function escapeStyleCode (code) {
+  return '\u001b[' + code + 'm'
 }
+
+function bold (text) {
+  var left = ''
+  var right = ''
+  const formatCodes = util.inspect.colors.bold
+  left += escapeStyleCode(formatCodes[0])
+  right = escapeStyleCode(formatCodes[1]) + right
+  return left + text + right
+}
+
+const vulnerableWarning = bold('The current Node.js version (' + process.version + ') is vulnerable to the following CVEs:')
+
+var separator = '='
+for (var i = 0; i < process.stdout.columns; ++i) {
+  separator = separator + '='
+}
+
+module.exports.danger = danger
+module.exports.allGood = allGood
+module.exports.bold = bold
+module.exports.vulnerableWarning = vulnerableWarning
+module.exports.separator = separator
